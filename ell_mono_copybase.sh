34faf8acaa2ba54fec01db47e814dcaf05ac2d94
anton-altarejos <altarejos.antoniolucas@gmail.com>
2026-02-06 23:54:11 +0800

    Add files via upload

---
+++ ell_mono_copybase.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE=ell_mdamb231.egsinp
+#FILE=build_ellipsoids.py
+
+cp ell_mono/* ell_mono_mdamb231_00/
+cp ell_mono/* ell_mono_mdamb231_90/
+cp ell_mono/* ell_mono_mdamb231_45/
+
+cp ell_mono/* ell_mono_mdamb361_00/
+cp ell_mono/* ell_mono_mdamb361_45/
+cp ell_mono/* ell_mono_mdamb361_90/
+
+cp ell_mono/* ell_mono_mdamb468_00/
+cp ell_mono/* ell_mono_mdamb468_45/
+cp ell_mono/* ell_mono_mdamb468_90/
---
+++ ell_rad_copybase.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE=ell_mdamb231.egsinp
+#FILE=build_ellipsoids.py
+
+cp ell_rad/* ell_rad_mdamb231_00/
+cp ell_rad/* ell_rad_mdamb231_90/
+cp ell_rad/* ell_rad_mdamb231_45/
+
+cp ell_rad/* ell_rad_mdamb361_00/
+cp ell_rad/* ell_rad_mdamb361_45/
+cp ell_rad/* ell_rad_mdamb361_90/
+
+cp ell_rad/* ell_rad_mdamb468_00/
+cp ell_rad/* ell_rad_mdamb468_45/
+cp ell_rad/* ell_rad_mdamb468_90/
